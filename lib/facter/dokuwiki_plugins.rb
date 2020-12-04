ec19553697b02d7c76c4dcabb7562f5355c48c4c
Mattias Giese <mattias.giese@giz.de>
2020-12-04 18:36:34 -0500

    fact: i have no idea what i'm doing

--- lib/facter/dokuwiki_plugins.rb
+++ lib/facter/dokuwiki_plugins.rb
@@ -1,8 +1,6 @@
 Facter.add(:dokuwiki_plugins) do
   confine :kernel => 'Linux'
   setcode do
-    Facter::Util::Resolution.exec('php /var/www/dokuwiki/bin/plugin.php extension list -f i').split("\n").each |plugin| do
-      plugin.split(' ')[0]
-    end
+    Facter::Util::Resolution.exec('php /var/www/dokuwiki/bin/plugin.php extension list -f i').split("\n").each { |plugin| plugin.split(' ')[0] }
   end
 end
