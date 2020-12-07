3f2d6ec303879d51c568e36e3c7386530a7fc44a
Mattias Giese <mattias.giese@giz.de>
2020-12-07 16:40:50 -0500

    Custom fact foo

--- lib/facter/dokuwiki_plugins.rb
+++ lib/facter/dokuwiki_plugins.rb
@@ -1,15 +1,17 @@
 Facter.add(:dokuwiki_plugins) do
   confine :kernel => 'Linux'
   setcode do
-    install_path = Facter.value(:dokuwiki_install_path) || '/var/www/dokuwiki'
-    installed_plugins = []
-    all_plugins = Facter::Util::Resolution.exec("php #{install_path}/bin/plugin.php extension list -f i").split("\n")
-    all_plugins.each_line do |p|
-      %r{\A(?<plugin>\S+) +(?<state>[ibgdu]+) +(?<date_updated>\d{4}-\d{2}-\d{2}) +(?<description>\S+)\z} =~ p
-      if plugin and state == 'i' then
-        installed_plugins.push(plugin)
+    install_path = Facter.value(:dokuwiki_install_path) || nil
+    if install_path then
+      installed_plugins = []
+      all_plugins = Facter::Util::Resolution.exec("php #{install_path}/bin/plugin.php extension list -f i").split("\n")
+      all_plugins.each do |p|
+        %r{\A(?<plugin>\S+) +(?<state>[ibgdu]+) +(?<date_updated>\d{4}-\d{2}-\d{2}) +(?<description>[\S ]+)\z} =~ p
+        if plugin and state == 'i' then
+          installed_plugins.push(plugin)
+        end
       end
+      installed_plugins
     end
-    installed_plugins
   end
 end
