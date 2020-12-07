4ea2c628167f388d56bd500d59c67d968ab49d91
Mattias Giese <mattias.giese@giz.de>
2020-12-07 16:25:17 -0500

    Playing around with custom facts

--- lib/facter/dokuwiki_plugins.rb
+++ lib/facter/dokuwiki_plugins.rb
@@ -1,6 +1,15 @@
 Facter.add(:dokuwiki_plugins) do
   confine :kernel => 'Linux'
   setcode do
-    Facter::Util::Resolution.exec('php /var/www/dokuwiki/bin/plugin.php extension list -f i').split("\n").each { |plugin| plugin.split(' ')[0] }
+    install_path = Facter.value(:dokuwiki_install_path) || '/var/www/dokuwiki'
+    installed_plugins = []
+    all_plugins = Facter::Util::Resolution.exec("php #{install_path}/bin/plugin.php extension list -f i").split("\n")
+    all_plugins.each_line do |p|
+      %r{\A(?<plugin>\S+) +(?<state>[ibgdu]+) +(?<date_updated>\d{4}-\d{2}-\d{2}) +(?<description>\S+)\z} =~ p
+      if plugin and state == 'i' then
+        installed_plugins.push(plugin)
+      end
+    end
+    installed_plugins
   end
 end
