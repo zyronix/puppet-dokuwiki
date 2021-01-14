2720ba1155e136efa98bae4d5825a2f981742860
Mattias Giese <mattias.giese@giz.de>
2021-01-14 15:49:45 -0500

    Minor fixes (remove extraneous whitespace, add newlines)

--- manifests/acl.pp
+++ manifests/acl.pp
@@ -5,16 +5,16 @@
 # @summary Create ACL rules in the acl.auth.php configuration file
 #
 # @example
-#   dokuwiki::acl { 'acl_line': 
+#   dokuwiki::acl { 'acl_line':
 #     namespace  => '*',
 #     group      => '@ALL',
 #     permission => '8',
 #   }
 #
 # @param namespace Specify the name for which this ACL rule applies. Use '*' to apply rule to all namespaces.
-# @param group Specify the group for which this ACL rule applies. 
+# @param group Specify the group for which this ACL rule applies.
 #   Start the group name with the '@' sign and use '*' to apply rule to all groups.
-# @param permission Specify the permission for this ACL rule. 
+# @param permission Specify the permission for this ACL rule.
 #   Possible permissions are: 1 for read, 2 edit, 4 create, 8 upload. See https://www.dokuwiki.org/acl for more information.
 define dokuwiki::acl (
   String $namespace,
